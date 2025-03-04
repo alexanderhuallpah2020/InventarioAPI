0b4b23e557433944ef5c913de63ea17db279d4db
Alexander Junior Huallpa Huaychani <alehuallpah@upt.pe>
2025-03-04 15:43:56 -0500

    Abstracciones CQRS

---
+++ src/DataConsulting.Inventory.Application/Abstractions/Messaging/IQuery.cs
@@ -0,0 +1,12 @@
+﻿using DataConsulting.Inventory.Domain.Primitives;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataConsulting.Inventory.Application.Abstractions.Messaging
+{
+    public interface IQuery<TResponse> : IRequest<Result<TResponse>>{}
+}
---
+++ src/DataConsulting.Inventory.Application/DependencyInjection.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataConsulting.Inventory.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddMediatR(configuration =>
+            {
+                configuration.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly);
+                //configuration.AddOpenBehavior(typeof(LoggingBehavior<,>));
+                //configuration.AddOpenBehavior(typeof(ValidationBehavior<,>));
+
+            });
+
+            //services.AddValidatorsFromAssembly(typeof(DependencyInjection).Assembly);
+
+            //services.AddTransient<PrecioService>();
+
+            return services;
+        }
+    }
+}
