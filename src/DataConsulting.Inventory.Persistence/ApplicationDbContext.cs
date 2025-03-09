3b455b5a2fd6c25e2d55c9c974a0715651b87124
Alexander Junior Huallpa Huaychani <ajhuallpa@dcp.pe>
2025-03-09 02:59:03 -0500

    Installacion de EF y Configuracion

---
+++ src/DataConsulting.Inventory.Persistence/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using DataConsulting.Inventory.Domain.Abstractions;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataConsulting.Inventory.Persistence
+{
+    public sealed class ApplicationDbContext : DbContext, IUnitOfWork
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options) 
+        {
+
+
+        }
+
+    }
+}
