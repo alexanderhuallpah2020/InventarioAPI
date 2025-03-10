a116d4f4a6ad7e01d25a01385b8fca08511496df
Alexander Junior Huallpa Huaychani <ajhuallpa@dcp.pe>
2025-03-09 23:28:12 -0500

    Concurrencia Optimista en net

--- src/DataConsulting.Inventory.Persistence/ApplicationDbContext.cs
+++ src/DataConsulting.Inventory.Persistence/ApplicationDbContext.cs
@@ -1,4 +1,6 @@
-﻿using DataConsulting.Inventory.Domain.Abstractions;
+﻿using DataConsulting.Inventory.Application.Exceptions;
+using DataConsulting.Inventory.Domain.Abstractions;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -10,16 +12,54 @@ namespace DataConsulting.Inventory.Persistence
 {
     public sealed class ApplicationDbContext : DbContext, IUnitOfWork
     {
-        public ApplicationDbContext(DbContextOptions options) : base(options) 
+        private readonly IPublisher _publisher;
+
+        public ApplicationDbContext(DbContextOptions options, IPublisher publisher) : base(options) 
         {
-
-
+            _publisher = publisher;
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
             base.OnModelCreating(modelBuilder);
+        }
+
+        public override async Task<int> SaveChangesAsync(
+        CancellationToken cancellationToken = default
+        )
+        {
+            try
+            {
+                var result = await base.SaveChangesAsync(cancellationToken);
+
+                await PublishDomainEventsAsync();
+
+                return result;
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                throw new ConcurrencyException("La excepcion por concurrencia se disparo", ex);
+            }
+        }
+
+        private async Task PublishDomainEventsAsync()
+        {
+            var domainEvents = ChangeTracker
+                .Entries<Entity>()
+                .Select(entry => entry.Entity)
+                .SelectMany(entity =>
+                {
+                    var domainEvents = entity.GetDomainEvents();
+                    entity.ClearDomainEvents();
+                    return domainEvents;
+                }).ToList();
+
+            foreach (var domainEvent in domainEvents)
+            {
+                await _publisher.Publish(domainEvent);
+            }
+
         }
 
     }
